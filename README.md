8ddc75648e878758ee6f24d415cbc40855955d51
wu529778790 <529778790@qq.com>
2020-02-15 20:44:20 +0800

    增加备注，修改readme

--- README.md
+++ README.md
@@ -2,13 +2,39 @@
 # vue-element-admin-express
 
 
-# 修改记录
+# 前端
+
   vue.config.js
 
   cheap-source-map
   调试的时候用  source-map   虽然每次都可以看见，但是生成文件较多，速度慢
+  
   开发的时候用eval     速度快，但是调试不方便,无法还原源码
-                
+
+  ## 路由
+
+  访问路由时会从 Cookie 中获取 Token，判断 Token 是否存在：
+
+  - 如果 Token 存在，将根据用户角色生成动态路由，然后访问路由，生成对应的页面组件。这里有一个特例，即用户访问 /login 时会重定向至 / 路由；
+
+  - 如果 Token 不存在，则会判断路由是否在白名单中，如果在白名单中将直接访问，否则说明该路由需要登录才能访问，此时会将路由生成一个 redirect 参数传入 login 组件，实际访问的路由为：/login?redirect=/xxx。
+
+  ## 动态路由和权限校验
+
+  - 路由分为：constantRoutes 和 asyncRoutes
+  - 用户登录系统时，会动态生成路由，其中 constantRoutes 必然包含，asyncRoutes 会进行过滤
+  - asyncRoutes 过滤的逻辑是看路由下是否包含 meta 和 meta.roles 属性，如果没有该属性，所以这是一个通用路由，不需要进行权限校验；如果包含 roles 属性则会判断用户的角色是否命中路由中的任意一个权限，如果命中，则将路由保存下来，如果未命中，则直接将该路由舍弃；
+  - asyncRoutes 处理完毕后，会和 constantRoutes 合并为一个新的路由对象，并保存到 vuex 的 permission/routes 中
+  - 用户登录系统后，侧边栏会从 vuex 中获取 state.permission.routes，根据该路由动态渲染用户菜单。
+
+  ### 路由权限校验
+  ![路由权限校验.png](https://s2.ax1x.com/2020/02/15/1zUjZ4.png)
+
+  ### 动态路由分析
+  ![动态路由分析.png](https://s2.ax1x.com/2020/02/15/1zwWqO.png)
+
+# 后端
+
 # 重点
 10-6 动态路由原理讲解
 
--- src/permission.js
+++ src/permission.js
@@ -6,49 +6,49 @@ import 'nprogress/nprogress.css' // progress bar style
 import { getToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
 
-NProgress.configure({ showSpinner: false }) // NProgress Configuration
+NProgress.configure({ showSpinner: false }) // showSpinner 可以控制右侧的环形进度条是否显示
 
 const whiteList = ['/login', '/auth-redirect'] // no redirect whitelist
 
 router.beforeEach(async(to, from, next) => {
-  // start progress bar
+  // 启动进度条
   NProgress.start()
 
-  // set page title
+  // 修改页面标题
   document.title = getPageTitle(to.meta.title)
 
-  // determine whether the user has logged in
+  // 从Cookie获取Token
   const hasToken = getToken()
-
+  // 判断Token是否存在
   if (hasToken) {
     if (to.path === '/login') {
-      // if is logged in, redirect to the home page
+      // 如果当前路径为login则直接重定向到首页
       next({ path: '/' })
       NProgress.done()
     } else {
-      // determine whether the user has obtained his permission roles through getInfo
+      //  判断用户角色存在，则直接访问
       const hasRoles = store.getters.roles && store.getters.roles.length > 0
       if (hasRoles) {
         next()
       } else {
         try {
-          // get user info
-          // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
+          // 异步获取用户的角色
           const { roles } = await store.dispatch('user/getInfo')
 
-          // generate accessible routes map based on roles
+          // 根据用户角色，动态生成路由
           const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
 
-          // dynamically add accessible routes
+          // 调用router.addRoutes动态加载路由
           router.addRoutes(accessRoutes)
 
-          // hack method to ensure that addRoutes is complete
-          // set the replace: true, so the navigation will not leave a history record
+          // 使用replace访问路由，不会再history中留下目录
           next({ ...to, replace: true })
         } catch (error) {
-          // remove token and go to login page to re-login
+          // 异常的话移除Token数据
           await store.dispatch('user/resetToken')
+          // 显示错误提示
           Message.error(error || 'Has Error')
+          // 重定向至登录页面
           next(`/login?redirect=${to.path}`)
           NProgress.done()
         }
@@ -56,12 +56,11 @@ router.beforeEach(async(to, from, next) => {
     }
   } else {
     /* has no token*/
-
+    // 如果访问的URL在白名单中，则直接访问
     if (whiteList.indexOf(to.path) !== -1) {
-      // in the free login whitelist, go directly
       next()
     } else {
-      // other pages that do not have permission to access are redirected to the login page.
+      // 如果访问的URL不在白名单中，则直接重定向到登录页面，并将访问的 URL 添加到 redirect 参数中
       next(`/login?redirect=${to.path}`)
       NProgress.done()
     }
@@ -69,6 +68,6 @@ router.beforeEach(async(to, from, next) => {
 })
 
 router.afterEach(() => {
-  // finish progress bar
+  // 停止进度条
   NProgress.done()
 })
