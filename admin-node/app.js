ed76a849864d25c68783ddaf16a6094e0008e994
wu529778790 <529778790@qq.com>
2020-02-15 16:59:43 +0800

    初始化后台node

---
+++ admin-node/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = require('./router')
+
+const app = express()
+
+app.use('/', router)
+
+const server = app.listen(5000, function() {
+  const { address, port } = server.address()
+  console.log('node is running on http://%s:%s', address, port)
+})
